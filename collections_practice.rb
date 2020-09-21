8eb966d7a3c9d5a3df56263544a4292156897f6c
Kimberly Baker <bakerk2010@gmail.com>
2020-09-21 03:58:19 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,17 @@
 
+def sort_array_asc(integers)
+  integers.sort
+end 
+
+def sort_array_desc(integers)
+  integers.sort{|a,b| b<=>a}
+end
+
+def sort_array_char_count(strings)
+  strings.sort{|a,b| a.length<=>b.length}
+end
+
+def swap_elements(array)
+  array.sort{|a,b| a[1]<=>b[2]}
+end
+
